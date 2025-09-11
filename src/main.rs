b7f274fed061b1e4b1acf1615e3b9bb089d9cd6d
uiriansan <uiriansan@gmail.com>
2025-09-11 14:46:51 -0300

    Project structure and enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use chafa::canvas::Canvas;
+use chafa::canvas::Config;
+
 fn main() {
+    let config = Config::new_detect();
+    let canvas = Canvas::new(&config.unwrap());
+    let _ = canvas.unwrap().create_string(None);
+
     println!("Hello, world!");
 }
